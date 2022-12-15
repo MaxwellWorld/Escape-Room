972b8b23daf5e5b00b948ec17f58d46a46666210
Maxwell <maxwellverso@outlook.com>
2022-12-15 15:16:03 -0500

    Control de movimiento
    Se actualiza el controlador del movimiento 3D, ya funciona el movimiento de cámara, para arriba y abajo y horizontal. Además se deja la cámara en primera persona.

--- Escape room/Assets/Scripts/PlayerMovement.cs
+++ Escape room/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float Speed = 1.0f;
-    public float RotationSpeed = 1.0f;
+    public Vector2 sensitivity;
     public float JumpForce = 1.0f;
+    public new Transform camera;
 
     private Rigidbody Physics;
 
@@ -18,21 +19,60 @@ public class NewBehaviourScript : MonoBehaviour
         Physics = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateMouseLook()
+    {
+        float Horizontal = Input.GetAxis("Mouse X");
+        float Vertical = Input.GetAxis("Mouse Y");
+
+        if (Horizontal != 0)
+        {
+            transform.Rotate(0, Horizontal * sensitivity.x, 0);
+        }
+
+        if (Vertical != 0)
+        {
+            Vector3 rotation = camera.localEulerAngles;
+            rotation.x = (rotation.x - Vertical * sensitivity.y + 360) % 360;
+            if (rotation.x > 80 && rotation.x < 180)
+            {
+                rotation.x = 80;
+            }
+            else if (rotation.x < 280 && rotation.x > 180)
+            {
+                rotation.x = 280;
+            }
+
+            camera.localEulerAngles = rotation;
+        }
+    }
+
+    private void UpdateMovement()
     {
         float Horizontal = Input.GetAxis("Horizontal");
         float Vertical = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector3(Horizontal, 0.0f, Vertical) * Time.deltaTime * Speed);
-        
-        float rotationY = Input.GetAxis("Mouse X");
+        Vector3 velocity = Vector3.zero;
 
-        transform.Rotate(new Vector3(0, rotationY * Time.deltaTime * RotationSpeed, 0));
+        if (Horizontal != 0 || Vertical != 0 )
+        {
+            Vector3 direction = (transform.forward * Vertical + transform.right * Horizontal).normalized;
+
+            velocity = direction * Speed;
+        }
+            velocity.y = Physics.velocity.y;
+            Physics.velocity = velocity;
+       
 
         if (Input.GetKeyDown(KeyCode.Space))
-        {
+        { 
             Physics.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
         }
     }
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+        UpdateMouseLook();
+    }
+
 }
